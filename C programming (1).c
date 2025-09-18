cb96c673443d16029499e27ccbaefa4ff2133be5
calistusares-star <calistusares@gmail.com>
2025-09-18 12:58:54 +0300

    Add files via upload

---
+++ C programming (1).c
@@ -0,0 +1,22 @@
+//Simple C program(Hello.C),Single line comment 
+/*
+Name:Ian Idalinya 
+Reg:CT100/G/26284/25
+Description: Hello C
+*/
+
+#include<stdio.h>
+
+//main function
+int main(){
+    printf("Hello.C\n");
+    printf("Analogue Electronics\n");
+    printf("Introduction to programming and Algorithms\n");
+    printf("Computer Systems and Organization\n");
+    printf("Introduction to Computer Application\n");
+    printf("Mathematics for Science\n");
+    printf("Fundamental of ICT\n");
+    printf("Essentials of Accounting\n");
+    printf("Communication and Information Literacy skills\n");
+    return 0;
+}
